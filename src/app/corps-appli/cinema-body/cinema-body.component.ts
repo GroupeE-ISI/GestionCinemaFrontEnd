ed98d0680e3322b5b35b3f5b8ee139f3cf07160c
sergio <serge.korsa@gmail.com>
2020-08-23 12:29:31 +0200

    Modification de format time et date

--- src/app/corps-appli/cinema-body/cinema-body.component.ts
+++ src/app/corps-appli/cinema-body/cinema-body.component.ts
@@ -1,6 +1,8 @@
 import { CinemaService } from './../../shared/service/cinema.service';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 
 @Component({
   selector: 'app-cinema-body',
