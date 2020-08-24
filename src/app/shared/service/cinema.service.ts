a11c20ddf6a912ddd7fea970f0caafa9e27a23e8
DESKTOP-EQT1561\IB <maregaissa@hotmail.com>
2020-08-24 22:17:23 +0200

    service observable cinema

--- src/app/shared/service/cinema.service.ts
+++ src/app/shared/service/cinema.service.ts
@@ -1,33 +1,35 @@
+import { Cinema } from './../Models/cinema.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CinemaService {
 
-    public recup:string="http://localhost:8080"
+    public recup = 'http://localhost:8080/cinema';
 
   constructor(public http: HttpClient) { }
 
-    public getVilles(){
-      return this.http.get(this.recup+"/villes");
+    public getCinema(): Observable<Cinema[]> {
+      return this.http.get<Cinema[]>(this.recup + '/lister');
     }
 
-    getCinemas(v){
-      return this.http.get(v._links.cinemas.href);
-    }
+    // getCinemas(v){
+    //   return this.http.get(v._links.cinemas.href);
+    // }
 
-    getSalles(c){
-      return this.http.get(c._links.salles.href);
+    // getSalles(c){
+    //   return this.http.get(c._links.salles.href);
 
-    }
+    // }
     
-    getSeances(salle:any){
-     let url = salle._links.seances.href.replace("{?projection}","");
-      return this.http.get(url+"?projection=p1");
+    // getSeances(salle:any){
+    //  let url = salle._links.seances.href.replace("{?projection}","");
+    //   return this.http.get(url+"?projection=p1");
 
-    } 
+    // } 
 
 
    }
